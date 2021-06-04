802ad027cc59a8358e1e07b2e57b3e119e9be074
LaBibine <sabine.moreira95@gmail.com>
2021-06-04 14:11:06 +0200

    changed onChange to a function

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -8,22 +8,22 @@ class SearchBar extends React.Component {
     this.handleInStockChange = this.handleInStockChange.bind(this)
   }
 
-  handleFilterTextChange (e) {
+  handleFilterTextChange(e) {
     this.props.onFilterTextChange(e.target.value)
   }
 
-  handleInStockChange (e) {
+  handleInStockChange(e) {
     this.props.onStockChange(e.target.checked)
   }
 
   render () {
     return <>
       <div className="form-group">
-        <input type="text" className="form-control" placeholder="Rechercher" onChange={this.handleFilterTextChange}/>
+        <input type="text" className="form-control" placeholder="Rechercher" onChange={() => this.handleFilterTextChange}/>
       </div>
       <div className="form-check">
         <label htmlFor="checkbox" className="form-check-label">Produit en stock seulement</label>
-        <input type="checkbox" className="form-check-input" id="stock" onChange={this.handleInStockChange}/>
+        <input type="checkbox" className="form-check-input" id="stock" onChange={ () => this.handleInStockChange}/>
       </div>
     </>
   }
